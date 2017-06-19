1b3f552893908d0798b25e18d9d3a8bc68826f95
Иван Иванов <ivanski34@gmail.com>
2017-06-20 01:08:44 +0300

    Add files via upload

---
+++ ByteMe/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bYteMe.Startup))]
+
+namespace bYteMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            this.ConfigureAuth(app);
+        }
+    }
+}
